0e56b9fd45994554433b1019a0a0a59c712281f4
daisi-app <clement1402@gmail.com>
2025-04-21 23:01:59 +0200

    Add .gitignore + add static DNS rules

---
+++ rules.sh
@@ -0,0 +1,26 @@
+# ********** DNS **********
+# ----- STATIC DNS ENTRIES -----
+#/ip dns static remove   A MODIFIER POUR SUPPRIMER TOUTES LES ENTREES DNS STATIC
+
+# Local entries
+/ip dns static add address=192.168.88.1 name=router.lan comment="Local Entries"
+/ip dns static add address=192.168.88.175 name=daisi.lan comment="Local Entries"
+
+# DNS DoH - CloudFlare
+/ip dns static add address=1.1.1.1 name=one.one.one.one comment="DNS DoH"
+
+# Blocked websites
+/ip dns static add address=127.0.0.1 match-subdomain=yes name=cokain.fr comment="Blocked websites"
+/ip dns static add address=127.0.0.1 match-subdomain=yes name=gamaniak.com comment="Blocked websites"
+/ip dns static add address=127.0.0.1 match-subdomain=yes name=tiktok.com comment="Blocked websites"
+/ip dns static add address=127.0.0.1 match-subdomain=yes name=webchoc.com comment="Blocked websites"
+
+# ----- DNS DoH CONFIGURATION -----
+# Add DNS certification
+#/certificate import file-name=CertificateFileName
+
+# Configure DNS entry
+#/ip dns set use-doh-server=https://one.one.one.one/dns-query verify-doh-cert=yes
+
+# Flush cache DNS to reset all
+/ip dns cache flush
